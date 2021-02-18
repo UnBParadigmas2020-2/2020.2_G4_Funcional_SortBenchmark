e9cd82788d1f7e9f5a2d8252de752d9bf80ad87b
EsioFreitas <esio.gustavo@gmail.com>
2021-02-18 13:08:26 -0300

    make aux list functions

--- GenerateList.hs
+++ GenerateList.hs
@@ -5,3 +5,9 @@ import System.Random (randomRIO)
 getListSizes :: [Int]
 getListSizes = map round [ 2 ** 10, 2 ** 12, 2 ** 14, 2 ** 16, 2 ** 18, 2 ** 20]
 
+getListSize :: Int -> Int
+getListSize x = if x >= length getListSizes then 0 else getListSizes !! x
+
+getBiggestSize :: Int
+getBiggestSize = last getListSizes
+
