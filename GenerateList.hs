1991dabc5ad8836b31bd90781ccf37ea287601c5
EsioFreitas <esio.gustavo@gmail.com>
2021-02-18 13:08:43 -0300

    add generateRandomList

--- GenerateList.hs
+++ GenerateList.hs
@@ -3,7 +3,7 @@ module GenerateList where
 import System.Random (randomRIO)
 
 getListSizes :: [Int]
-getListSizes = map round [ 2 ** 10, 2 ** 12, 2 ** 14, 2 ** 16, 2 ** 18, 2 ** 20]
+getListSizes = map round [2 ** 10, 2 ** 12, 2 ** 14, 2 ** 16, 2 ** 18, 2 ** 20]
 
 getListSize :: Int -> Int
 getListSize x = if x >= length getListSizes then 0 else getListSizes !! x
@@ -11,3 +11,9 @@ getListSize x = if x >= length getListSizes then 0 else getListSizes !! x
 getBiggestSize :: Int
 getBiggestSize = last getListSizes
 
+generateRandomList :: Int -> IO [Int]
+generateRandomList 0 = return []
+generateRandomList n = do
+  r <- randomRIO (0, getBiggestSize)
+  rs <- generateRandomList (n -1)
+  return (r : rs)
