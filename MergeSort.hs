27a784771d62d93f9620bc6de5f808c0f07b6aa1
phe0 <pheofo@gmail.com>
2021-02-19 12:02:59 -0300

    add function to merge two arrays

--- MergeSort.hs
+++ MergeSort.hs
@@ -5,3 +5,10 @@ getHalf :: Ord a => [a] -> Int -> [a]
 getHalf a valor 
                 | valor == 1 = take (div (length a) 2) a
                 | valor == 2 = drop (div (length a) 2) a
+
+merge :: Ord a => [a] -> [a] -> [a]
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys)
+                | x <= y    = x : merge xs (y:ys)
+                | otherwise = y : merge (x:xs) ys
