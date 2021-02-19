b633fdd1c1407c4840dc776aae7d90024d99ffe0
phe0 <pheofo@gmail.com>
2021-02-19 12:25:36 -0300

    add function to count and check when to stopping 'bubbling up'

--- BubbleSort.hs
+++ BubbleSort.hs
@@ -2,3 +2,8 @@ bubbleUp :: Ord a => [a] -> [a]
 bubbleUp [] = []
 bubbleUp [a] = [a]
 bubbleUp (x:y:xs) = min x y : bubbleUp (max x y : xs)
+
+bubbleCounter :: Ord a => [a] -> Int -> [a]
+bubbleCounter xs i 
+            | i == length xs = xs
+            | otherwise = bubbleCounter (bubbleUp xs) (i + 1)
