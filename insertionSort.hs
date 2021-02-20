34b1a71a69bb5062dbae21813a7d1957d404792c
MarcosNBJ <marcosnery.comp@gmail.com>
2021-02-20 03:03:43 -0300

    creating InsertionSort module

--- insertionSort.hs
+++ insertionSort.hs
@@ -1,8 +1,10 @@
+module InsertionSort(insertionSort) where
+
 insert :: Ord a => a -> [a] -> [a]
 insert x [] = [x]
 insert x (y:ys) | x < y     = x:y:ys
                 | otherwise = y:(insert x ys)
 
-insertSort :: Ord a => [a] -> [a]
-insertSort [] = []
-insertSort (x:xs) = insert x (insertSort xs)
+insertionSort :: Ord a => [a] -> [a]
+insertionSort [] = []
+insertionSort (x:xs) = insert x (insertionSort xs)
