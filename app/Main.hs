4891bd63cded78a419dd9acbef418a82a4e6a730
Lucas Dutra <ldutra98@gmail.com>
2021-02-25 20:11:26 -0300

    prototype for benchmarking
    Co-Authored-By: Marcos Nery <marcosnery.comp@gmail.com>

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,29 @@
 import GenerateList
+import InsertionSort (insertionSort)
 
-main :: IO ()
+import Text.Printf
+import Control.Exception
+import System.CPUTime
+
+printBenchmark timeSpent maxTime
+  |timeSpent <= maxTime = printf "Tempo levado: %0.3f sec\n" (timeSpent)
+  |otherwise = printf "time limit exceeded\n"
+
+time :: IO t  -> Double -> IO t
+time a maxTime = do
+    start <- getCPUTime
+    v <- a
+    end   <- getCPUTime
+    let diff = (fromIntegral (end - start)) / (10^12)
+    printBenchmark (diff :: Double) (maxTime)
+    return v
+
+benchmarkForAllArrays algorithm maxTime = do 
+    lighterList <- generateRandomList (getListSize 0)
+    time ((algorithm lighterList) `seq` return ()) maxTime
+    heavyList <- generateRandomList (getListSize 3)
+    time ((algorithm heavyList) `seq` return ()) maxTime
+
 main = do
-  putStrLn "Digite o tempo máximo em milissegundos:"
-  maxTime <- getLine
-  let lighterList = generateRandomList (getListSize 0)
-  let lightList = generateRandomList (getListSize 1)
-  let mediumList = generateRandomList (getListSize 2)
-  let heavyList = generateRandomList (getListSize 3)
-  let heavierList = generateRandomList (getListSize 4)
-  let veryHeavierList = generateRandomList (getListSize 5)
-
-  print "lighterList"
+    maxTime <- getLine
+    benchmarkForAllArrays insertionSort (read maxTime :: Double)
