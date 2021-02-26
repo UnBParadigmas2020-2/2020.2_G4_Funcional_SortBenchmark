f129432cc6a3e156f9b837b2596f0cdfa287337b
Lucas Dutra <ldutra98@gmail.com>
2021-02-26 17:00:05 -0300

    Insert arrays dynamically

--- app/CalculateTime.hs
+++ app/CalculateTime.hs
@@ -8,7 +8,7 @@ import System.CPUTime
 
 printBenchmark timeSpent maxTime
   |timeSpent <= maxTime = printf "Tempo levado: %0.3f sec\n" (timeSpent)
-  |otherwise = printf "time limit exceeded\n"
+  |otherwise = printf "Tempo maximo excedido! Tempo levado: %0.3f sec\n" (timeSpent)
 
 time :: IO t  -> Double -> IO t
 time a maxTime = do
@@ -19,16 +19,5 @@ time a maxTime = do
     printBenchmark (diff :: Double) (maxTime)
     return v
 
-benchmarkForAllArrays algorithm maxTime = do 
-    lighterList <- generateRandomList (getListSize 0)
-    time ((algorithm lighterList) `seq` return ()) maxTime
-    lightList <- generateRandomList (getListSize 1)
-    time ((algorithm lightList) `seq` return ()) maxTime
-    mediumList <- generateRandomList (getListSize 2)
-    time ((algorithm mediumList) `seq` return ()) maxTime
-    heavyList <- generateRandomList (getListSize 3)
-    time ((algorithm heavyList) `seq` return ()) maxTime
-    heavierList <- generateRandomList (getListSize 4)
-    time ((algorithm heavierList) `seq` return ()) maxTime
-    veryHeavierList <- generateRandomList (getListSize 5)
-    time ((algorithm veryHeavierList) `seq` return ()) maxTime
+benchmarkForAllArrays algorithm randomList maxTime = do 
+    time ((algorithm randomList) `seq` return ()) maxTime
--- app/Main.hs
+++ app/Main.hs
@@ -7,14 +7,46 @@ import QuickSort(quickSort)
 import ShellSort(shellSort)
 
 main = do
+    putStrLn "\n\nDigite, em segundos, o tempo limite para execucao do algoritmo! \nInclui numeros decimais separados por (.)"
     maxTime <- getLine
-    print "-------------insertion---------------"
-    benchmarkForAllArrays insertionSort (read maxTime :: Double)
-    print "-------------bubble---------------"
-    benchmarkForAllArrays bubbleSort (read maxTime :: Double)
-    print "-------------merge---------------"
-    benchmarkForAllArrays mergeSort (read maxTime :: Double)
-    print "-------------quick---------------"
-    benchmarkForAllArrays quickSort (read maxTime :: Double)
-    print "-------------shell---------------"
-    benchmarkForAllArrays shellSort (read maxTime :: Double)
+    lighterList <- generateRandomList (getListSize 0)
+    lightList <- generateRandomList (getListSize 1)
+    mediumList <- generateRandomList (getListSize 2)
+    heavyList <- generateRandomList (getListSize 3)
+    heavierList <- generateRandomList (getListSize 4)
+    veryHeavierList <- generateRandomList (getListSize 5)
+    putStrLn "-------------insertion---------------"
+    benchmarkForAllArrays insertionSort lighterList (read maxTime :: Double)
+    benchmarkForAllArrays insertionSort lightList (read maxTime :: Double)
+    benchmarkForAllArrays insertionSort mediumList (read maxTime :: Double)
+    benchmarkForAllArrays insertionSort heavyList (read maxTime :: Double)
+    benchmarkForAllArrays insertionSort heavierList (read maxTime :: Double)
+    benchmarkForAllArrays insertionSort veryHeavierList (read maxTime :: Double)
+    putStrLn "\n\n-------------bubble---------------"
+    benchmarkForAllArrays bubbleSort lighterList (read maxTime :: Double)
+    benchmarkForAllArrays bubbleSort lightList (read maxTime :: Double)
+    benchmarkForAllArrays bubbleSort mediumList (read maxTime :: Double)
+    benchmarkForAllArrays bubbleSort heavyList (read maxTime :: Double)
+    benchmarkForAllArrays bubbleSort heavierList (read maxTime :: Double)
+    benchmarkForAllArrays bubbleSort veryHeavierList (read maxTime :: Double)
+    putStrLn "\n\n-------------merge---------------"
+    benchmarkForAllArrays mergeSort lighterList (read maxTime :: Double)
+    benchmarkForAllArrays mergeSort lightList (read maxTime :: Double)
+    benchmarkForAllArrays mergeSort mediumList (read maxTime :: Double)
+    benchmarkForAllArrays mergeSort heavyList (read maxTime :: Double)
+    benchmarkForAllArrays mergeSort heavierList (read maxTime :: Double)
+    benchmarkForAllArrays mergeSort veryHeavierList (read maxTime :: Double)
+    putStrLn "\n\n-------------quick---------------"
+    benchmarkForAllArrays quickSort lighterList (read maxTime :: Double)
+    benchmarkForAllArrays quickSort lightList (read maxTime :: Double)
+    benchmarkForAllArrays quickSort mediumList (read maxTime :: Double)
+    benchmarkForAllArrays quickSort heavyList (read maxTime :: Double)
+    benchmarkForAllArrays quickSort heavierList (read maxTime :: Double)
+    benchmarkForAllArrays quickSort veryHeavierList (read maxTime :: Double)
+    putStrLn "\n\n-------------shell---------------"
+    benchmarkForAllArrays shellSort lighterList (read maxTime :: Double)
+    benchmarkForAllArrays shellSort lightList (read maxTime :: Double)
+    benchmarkForAllArrays shellSort mediumList (read maxTime :: Double)
+    benchmarkForAllArrays shellSort heavyList (read maxTime :: Double)
+    benchmarkForAllArrays shellSort heavierList (read maxTime :: Double)
+    benchmarkForAllArrays shellSort veryHeavierList (read maxTime :: Double)
